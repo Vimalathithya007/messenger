4fe950d9da865b5845d1aee9f6909b0b4f98ad1d
Vimal Athithya <96301970+Vimalathithya007@users.noreply.github.com>
2024-07-08 23:15:51 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
 const express = require("express")
 
 const mongoose = require('mongoose')
+
+const colors=require('colors');
+const userRoutes=require('./routes/userRoutes')
+const chatRoutes=require('./routes/chatRoutes')
+const {notFound,errorHandler}=require('./middleware/errorMiddleware.js')
+
+
+dotenv.config();
+//connectDB();
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
+const db=mongoose.connection
+db.on('error',error => console.error(error))
+db.once('open',() => console.log('Connected to Mongoose'))
